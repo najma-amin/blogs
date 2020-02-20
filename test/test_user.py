919b283e11fd46803d2b4b4eb1b75e45184f44c8
najmaA <najmaamin10@gmai.com>
2020-02-20 10:42:00 +0300

    background image style in main.css

--- test/test_user.py
+++ test/test_user.py
@@ -5,7 +5,7 @@ from app.models import User
 class UserModelTest(unittest.TestCase):
 
     def setUp(self):
-        self.new_user = User(password = 'steve')
+        self.new_user = User(password = 'najma')
 
     def test_password_setter(self):
         self.assertTrue(self.new_user.pass_secure is not None)
@@ -15,5 +15,5 @@ class UserModelTest(unittest.TestCase):
                 self.new_user.password
 
     def test_password_verification(self):
-        self.assertTrue(self.new_user.verify_password('steve'))
+        self.assertTrue(self.new_user.verify_password('najma'))
         
